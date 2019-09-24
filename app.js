e22bfd653f00a30007cbde4edcbd548db447d684
Shubhan <ronyshv@gmail.com>
2019-09-23 22:34:56 -0400

    Create app.js

---
+++ app.js
@@ -0,0 +1,39 @@
+// setTimeout(() => {
+//     console.log('5 seconds passed')
+// }, 5000);
+
+// var time = 0;
+// var timer = setInterval(() => {
+//     time +=2;
+//     console.log(`${time} seconds gone`)
+//     if (time >=10) {
+//         clearInterval(timer)
+//     }
+// }, 2000);
+
+//Normal Function statement
+// function sayHI() {
+//     console.log('Hi')
+// }
+// sayHI();
+
+//Function Expression
+//Example 01
+// var myName = 'Rony';
+// var name = function () {
+//     console.log(`My name is ${myName}`)
+// }
+
+// name()
+
+//Example 02
+var nickName = 'Rony'
+function callFuntion(name) {
+    name();
+}
+
+var myName = function () {
+    console.log(`My name is ${nickName}`)
+}
+
+callFuntion(myName)
